95932145937ea5f68e808c9e5dcfe7658f57630c
Jeff P <jeffhparrish@gmail.com>
2018-11-27 14:41:27 -0800

    Update job.js

--- src/job.js
+++ src/job.js
@@ -1,7 +1,7 @@
 const Promise = require('bluebird');
 const debug = require('debug');
 
-module.exports = class Job {
+class Job {
   constructor(worker, tube, id) {
     this.worker = worker;
     this.tube = tube;
@@ -47,3 +47,5 @@ Job.status = function (stats) {
   if (!stats) return 'success';
   return stats.state;
 };
+
+module.exports = Job;
